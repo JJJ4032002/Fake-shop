7ffae0d799ab89326972fe44204182f2013d2c4c
JJJ4032002 <rp4888888@gmail.com>
2021-07-24 15:30:57 +0530

    Main page made for desktop

---
+++ src/components/MainPageText.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainPageText() {
+  return (
+    <div className="MainPageText">
+      <h1 className="MainPageHead">Buy all your clothes only at FAKESHOP</h1>
+      <button className="MainPageBtn">Shop Now</button>
+    </div>
+  );
+}
+
+export default MainPageText;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import cartLogo from "../Photos/icons8-shopping-cart-30.png";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">
+        <h1 id="NavHead" className="NavItems">
+          FAKESHOP
+        </h1>
+      </Link>
+      <div id="RightNav" className="NavItems">
+        <button className="RightNavItem" id="ShopBtn">
+          SHOP
+        </button>
+        <img className="RightNavItem" src={cartLogo} alt="cartLogo" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
