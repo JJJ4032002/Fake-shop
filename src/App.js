52345917acb9d8077d7ed0c3cf246676e688b0d1
JJJ4032002 <rp4888888@gmail.com>
2021-07-25 18:47:17 +0530

    new photos added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainPageText from "./components/MainPageText";
 import Shop from "./components/Shop";
+import Products from "./components/Products";
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={MainPageText} />
           <Route path="/shop" component={Shop} />
+          <Route path="/Product/:id" component={Products} />
         </Switch>
       </div>
     </Router>
